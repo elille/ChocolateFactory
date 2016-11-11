af647aed6dfe2901a1afd8951280ffd53f3e998a
Jarl Victor Bjørgan <jarlvict@stud.ntnu.no>
2016-11-11 15:10:36 +0100

    snurre animasjon på karriere

---
+++ js/peppermintScript.js
@@ -0,0 +1,27 @@
+// Alle peppermintene
+const candies = [
+document.getElementById('leftY'), 
+document.getElementById('leftG'),
+document.getElementById('leftR'),
+document.getElementById('leftP'),
+document.getElementById('rightY'),
+document.getElementById('rightG'),
+document.getElementById('rightR'),
+document.getElementById('rightP')];
+
+
+let degrees = 5;
+setInterval(rotate, 50);
+
+// Kjører en vri på alle bildene
+function rotate(){
+	for(var i = 0; i < candies.length; i++){
+		turnImg(candies[i]);
+	}
+	degrees += 2;
+}
+
+// vrir et enkelt bilde
+function turnImg(img){
+	img.style.transform = "rotate(" + degrees + "deg)";
+}
