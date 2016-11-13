11ad6158578e1ea8815824669c6279d8b51e5133
siljr <silje.riseng@gmail.com>
2016-11-13 20:34:53 +0100

    fixed duplcate code

---
+++ js/navbar.js
@@ -0,0 +1,26 @@
+document.write('\
+  <link rel="stylesheet" type="text/css" href="../css/mainStyle.css" />\
+  <!-- start code for navbar -->\
+  <!-- her har jeg brukt http://www.w3schools.com/css/css_navbar.asp  -->\
+  <nav class=main-nav>\
+    <!-- gjør at bildet av logoen blir en lenke til index.html -->\
+    <div class="logo">\
+      <a href="../index.html">\
+          <img src="../img/logo.svg" alt="Logo">\
+      </a>\
+    </div>\
+    <!--  liste med lenker som ligger i navbaren -->\
+    <div>\
+      <a class="square" target="_top" href="omFabrikken.html">OM OSS</a>\
+      <a class="square" target="_top" href="karriere.html">JOBB</a>\
+      <a class="square" target="_top" href="charities.html">VELDEDIGHET</a>\
+      <a class="square" target="_top" href="konkurranse.html">KONKURRANSER</a>\
+      <a class="square" target="_top" href="products.html">PRODUKTER</a>\
+    </div>\
+  </nav>\
+  <!-- end code for navbar -->\
+  \
+');
+
+
+//include to html from http://stackoverflow.com/questions/8988855/include-another-html-file-in-a-html-file
